3bc18014deb6394056235d2d70aee8e8a78e407a
Thomas Chaplin <tomrchaplin@gmail.com>
2023-01-09 09:13:07 +0000

    Abstract out ph computation after collecting cols

--- grpphati/pipelines/grounded.py
+++ grpphati/pipelines/grounded.py
@@ -4,13 +4,12 @@ from grpphati.homologies import (
     RegularPathHomology,
     Homology,
 )
-from grpphati.utils.column import convert_to_sparse
-from grpphati.utils.diagram import add_paired, add_unpaired
 from grpphati.optimisations import (
     all_optimisations,
     component_appendage_empty,
     component_empty,
 )
+from grpphati.utils.ph import compute_ph_from_cols
 from typing import Type
 import phat
 
@@ -49,30 +48,7 @@ def compute_grounded_ph(
     )
     if verbose:
         print(f"Filtration basis has size {len(cols)}")
-    # Sort basis according to entrance time and dimension
-    # (if dim1 and dim2 item enter at same time, the dim1 should enter first to have a valid filtration)
-    if verbose:
-        print("Sorting basis")
-    cols.sort(key=lambda col: (col.entrance_time, col.dimension()))
-    # Convert to sparse format for PHAT
-    if verbose:
-        print("Building sparse matrix")
-    sparse_cols = convert_to_sparse(cols)
-    # Compute persistence pairs
-    if verbose:
-        print("Computing PH")
-    boundary_matrix = phat.boundary_matrix(
-        columns=sparse_cols, representation=phat.representations.sparse_pivot_column
-    )
-    pairs = boundary_matrix.compute_persistence_pairs(reduction=reduction)
-    pairs.sort()
-    # Add finite points to persistence diagram, recovering times
-    if verbose:
-        print("Building diagram")
-    dgm = add_paired(pairs, cols)
-    # Any unpaired edges must give rise to infinite features
-    dgm = add_unpaired(dgm, pairs, cols)
-    return dgm
+    return compute_ph_from_cols(cols, verbose, reduction)
 
 
 GrPPH = make_grounded_pipeline(
