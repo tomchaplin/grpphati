d52d590e69213b884cb7b1420a0a31e4b01d875e
Thomas Chaplin <tomrchaplin@gmail.com>
2023-02-02 14:13:29 +0000

    Use `get_entrance_time()` when building `Result`

--- grpphati/results/result.py
+++ grpphati/results/result.py
@@ -12,8 +12,8 @@ class Result:
 
     def add_paired(self, pairs, cols):
         for [birth_idx, death_idx] in pairs:
-            birth_time = cols[birth_idx].entrance_time
-            death_time = cols[death_idx].entrance_time
+            birth_time = cols[birth_idx].get_entrance_time()
+            death_time = cols[death_idx].get_entrance_time()
             # Don't add 0 persistence points?
             if birth_time == death_time:
                 continue
@@ -28,7 +28,7 @@ class Result:
         dim_1_idxs = [idx for idx, col in enumerate(cols) if col.dimension() == 1]
         unpaired_idxs = [idx for idx in dim_1_idxs if idx not in all_paired_idxs]
         for idx in unpaired_idxs:
-            birth_time = cols[idx].entrance_time
+            birth_time = cols[idx].get_entrance_time()
             self.add_bar([birth_time, np.inf])
 
     def extend(self, other_result):
